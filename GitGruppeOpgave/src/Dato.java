3998a06f0d891fcca9bca2a564523d6ff6913434
jakobmajiddd <jakobmajid00@gmail.com>
2021-04-13 11:40:04 +0200

    Jeg har ændret meget

--- GitGruppeOpgave/src/Dato.java
+++ GitGruppeOpgave/src/Dato.java
@@ -2,6 +2,7 @@ public class Dato {
 
     public static void main(String[] args) {
 
-        System.out.println("Tirsdag d. 13 April");
+        System.out.println("Tirsdag d. 13 April :-)");
+
     }
 }
